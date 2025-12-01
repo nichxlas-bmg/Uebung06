e244a89f53e3b0406ca6837e1010a0a7f0f4e76d
nichxlas-bmg <zzxn6tz57x@privaterelay.appleid.com>
2025-12-01 10:56:35 +0100

    2

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -1,5 +1,25 @@
 package h1;
 
 public class H1_Main {
+public static void main (String[] args) {
+	int[] a = {1, 2, 3, 4};
+    int[] b = {4, 3, 2, 1}; 
+    
+   
+}
 
+public static boolean isMirrorArray(int[]a, int[] b) {
+	if (a.length == b.length) {
+		for(int i = 0; i < a.length; i++) {
+			if(a[i]!= b[b.length-1-i]) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	} else{
+		return false;
+	}
+	
+}
 }
